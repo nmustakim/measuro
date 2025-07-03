84acc82d0ce5cf0ecf233193f68efc773b02715c
nmustakim <nmustakim98@gmail.com>
2025-07-03 16:42:11 +0600

    ndk version updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.softhusiast.measuro"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 24
+        ndkVersion = "27.0.12077973"
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
